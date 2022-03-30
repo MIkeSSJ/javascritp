21f6a3595bb81e09df5f4599828a8a67e154c60a
Mike <74191867+MIkeSSJ@users.noreply.github.com>
2022-03-30 13:08:55 -0300

    Add files via upload

---
+++ ProjetoJS/2-trabalhandoComVariaveis.js
@@ -0,0 +1,13 @@
+console.log("trabalhando com variavéis");
+
+//JS é case sensitive
+
+const idade = 29;
+const nome = "mike";
+let ano = 2022;
+
+
+console.log("nome é " + nome + " idade é " + idade + " estamos no ano de " + ano);
+
+//não fazer
+//mes="janeiro"
---
+++ ProjetoJS/5-atribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+console.log("trabalhando com Atribuição de variavéis");
+
+//const idade = 29;
+const nome = "mike";
+const sobrenome = "queiroz";
+
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+let idade;
+idade = 26;
+idade = idade + 1;
+console.log(idade);
